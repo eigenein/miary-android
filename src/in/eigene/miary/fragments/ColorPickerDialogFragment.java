e0c596eb03cd5361689c8f36d41d145369018998
Pavel Perestoronin <eigenein@gmail.com>
2014-05-26 02:28:39 +0300

    #49 Fix title <-> message

--- src/in/eigene/miary/fragments/ColorPickerDialogFragment.java
+++ src/in/eigene/miary/fragments/ColorPickerDialogFragment.java
@@ -37,13 +37,10 @@ public class ColorPickerDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        // http://stackoverflow.com/a/13862509/359730
-        final ContextThemeWrapper context = new ContextThemeWrapper(
-                getActivity(), R.style.Miary_Theme_Dialog);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         final View view = getActivity().getLayoutInflater().inflate(R.layout.choose_color_fragment, null);
         builder.setView(view);
-        builder.setMessage(R.string.choose_color_message);
+        builder.setTitle(R.string.dialog_colorpicker_title);
         setListeners(view);
         return builder.create();
     }
