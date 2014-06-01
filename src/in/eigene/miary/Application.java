7a57abf2fc16872eb29ced0af646927a0435fc74
Pavel Perestoronin <eigenein@gmail.com>
2014-06-02 02:53:06 +0300

    #57: Temporarily disable default ACL

--- src/in/eigene/miary/Application.java
+++ src/in/eigene/miary/Application.java
@@ -1,6 +1,5 @@
 package in.eigene.miary;
 
-import com.parse.*;
 import in.eigene.miary.helpers.*;
 
 public class Application extends android.app.Application {
@@ -8,6 +7,5 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         ParseHelper.initialize(this);
-        ParseACL.setDefaultACL(new ParseACL(), true);
     }
 }
