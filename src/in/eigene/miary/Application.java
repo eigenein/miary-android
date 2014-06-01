f1fe2b024e26b1cf947d2cf91b190268ed4be77d
Pavel Perestoronin <eigenein@gmail.com>
2014-06-01 19:12:21 +0300

    Remove unused if statement

--- src/in/eigene/miary/Application.java
+++ src/in/eigene/miary/Application.java
@@ -8,9 +8,6 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         ParseHelper.initialize(this);
-
-        if (ParseUser.getCurrentUser() != null) {
-            ParseACL.setDefaultACL(new ParseACL(), true);
-        }
+        ParseACL.setDefaultACL(new ParseACL(), true);
     }
 }
