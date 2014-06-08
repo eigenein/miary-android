3a48e5976b9c250b85d36b48a5079649354dcbec
Pavel Perestoronin <eigenein@gmail.com>
2014-06-08 08:49:51 +0300

    #15 Initial code

--- src/in/eigene/miary/core/Substitutions.java
+++ src/in/eigene/miary/core/Substitutions.java
@@ -1,7 +1,53 @@
 package in.eigene.miary.core;
 
 /**
- * Created by eigenein on 08.06.2014.
+ * Automatic substitutions helper.
  */
 public class Substitutions {
+
+    public static class TableEntry {
+        public final String regularExpression;
+        public final String replacement;
+
+        public TableEntry(final String regularExpression, final String replacement) {
+            this.regularExpression = regularExpression;
+            this.replacement = replacement;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s → %s", regularExpression, replacement);
+        }
+    }
+
+    public static final String[] REPRS;
+
+    /**
+     * Replacement table.
+     */
+    private static final TableEntry[] TABLE = new TableEntry[] {
+            new TableEntry("--", "—"),
+            new TableEntry(" - ", "—"),
+            new TableEntry(">>", "»"),
+            new TableEntry("<<", "«"),
+            new TableEntry("...", "…")
+    };
+
+    /**
+     * Generates string representation of table entries.
+     */
+    static {
+        REPRS = new String[TABLE.length];
+        for (int i = 0; i < TABLE.length; i += 1) {
+            REPRS[i] = TABLE[i].toString();
+        }
+    }
+
+    public static String replace(final String string) {
+        String result = string;
+        for (final TableEntry entry : TABLE) {
+            result = result.replace(entry.regularExpression, entry.replacement);
+        }
+        return result;
+    }
 }
--- src/in/eigene/miary/fragments/SettingsFragment.java
+++ src/in/eigene/miary/fragments/SettingsFragment.java
@@ -1,8 +1,11 @@
 package in.eigene.miary.fragments;
 
+import android.app.*;
+import android.content.*;
 import android.os.*;
 import android.preference.*;
 import in.eigene.miary.*;
+import in.eigene.miary.core.*;
 import in.eigene.miary.core.export.*;
 
 public class SettingsFragment extends PreferenceFragment {
@@ -22,6 +25,26 @@ public class SettingsFragment extends PreferenceFragment {
                         return true;
                     }
         });
+
+        findPreference(R.string.prefkey_substitution_table).setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(final Preference preference) {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle(R.string.dialog_replacement_table_title);
+                        builder.setItems(Substitutions.REPRS, null);
+                        builder.setCancelable(true);
+                        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(final DialogInterface dialog, final int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.show();
+                        return true;
+                    }
+                }
+        );
     }
 
     private Preference findPreference(final int keyResourceId) {
