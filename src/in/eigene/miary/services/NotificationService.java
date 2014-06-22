b365e612b95c0cf707f88badc8cc691b58cbcf49
Andrei Menkou <nothingelsematters7@gmail.com>
2014-06-22 15:51:53 +0300

    #13 Implement scheduled notifications about note creation. There is only left implementation of scheduling alarms on device booting.

---
+++ src/in/eigene/miary/services/NotificationService.java
@@ -0,0 +1,54 @@
+package in.eigene.miary.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import in.eigene.miary.R;
+import in.eigene.miary.activities.FeedActivity;
+import in.eigene.miary.core.ReminderManager;
+
+import java.util.Calendar;
+
+public class NotificationService extends Service {
+
+    private static final String LOG_TAG = NotificationService.class.getSimpleName();
+
+    private static final int REMIND_NOTIFICATION_ID = 1;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setAutoCancel(true)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(getString(R.string.notification_reminder_content_title));
+
+
+        if (!ReminderManager.isReminderDay(this, Calendar.getInstance())) {
+            Log.i(LOG_TAG, "Not a reminder day. Skip notification creation");
+            return START_STICKY;
+        }
+
+        Intent myIntent = new Intent(this, FeedActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, myIntent, 0);
+
+        mBuilder.setContentIntent(contentIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(REMIND_NOTIFICATION_ID, mBuilder.build());
+
+        Log.i(LOG_TAG, "Reminder notification created");
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
