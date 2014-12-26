e847216fa59d1ce43069ab6889f3a1a7c00aee5f
Pavel Perestoronin <eigenein@gmail.com>
2014-12-27 01:11:47 +0300

    #147 Sending feedback

--- src/in/eigene/miary/activities/FeedbackActivity.java
+++ src/in/eigene/miary/activities/FeedbackActivity.java
@@ -3,7 +3,9 @@ package in.eigene.miary.activities;
 import android.content.*;
 import android.os.*;
 import android.view.*;
+import android.widget.*;
 import in.eigene.miary.*;
+import in.eigene.miary.core.classes.*;
 
 public class FeedbackActivity extends FullscreenDialogActivity {
 
@@ -23,5 +25,27 @@ public class FeedbackActivity extends FullscreenDialogActivity {
     public boolean onCreateOptionsMenu(final Menu menu) {
         getMenuInflater().inflate(R.menu.feedback_activity, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_feedback_send:
+                final String email = ((EditText)findViewById(R.id.feedback_email)).getText().toString();
+                final String text = ((EditText)findViewById(R.id.feedback_text)).getText().toString();
+                if (!text.isEmpty()) {
+                    final Feedback feedback = new Feedback().setText(text);
+                    if (!email.isEmpty()) {
+                        feedback.setEmail(email);
+                    }
+                    feedback.saveEventually();
+                    Toast.makeText(this, R.string.toast_thank_you, Toast.LENGTH_SHORT).show();
+                }
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- src/in/eigene/miary/core/classes/Feedback.java
+++ src/in/eigene/miary/core/classes/Feedback.java
@@ -5,9 +5,20 @@ import com.parse.*;
 @ParseClassName("Feedback")
 public class Feedback extends ParseObject {
 
+    private static final String KEY_EMAIL = "email";
     private static final String KEY_TEXT = "text";
 
     public Feedback() {
         // Do nothing.
+    }
+
+    public Feedback setEmail(final String email) {
+        put(KEY_EMAIL, email);
+        return this;
+    }
+
+    public Feedback setText(final String text) {
+        put(KEY_TEXT, text);
+        return this;
     }
 }
