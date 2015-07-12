d67fa21924fb7a11cfbc9213af5ad8aff21ca771
Pavel Perestoronin <eigenein@gmail.com>
2015-07-12 18:59:33 +0300

    #202 Fix saveNote issue when switching fullscreen mode.

--- app/src/main/java/in/eigene/miary/fragments/NoteFragment.java
+++ app/src/main/java/in/eigene/miary/fragments/NoteFragment.java
@@ -155,8 +155,8 @@ public class NoteFragment extends BaseFragment {
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onPause() {
+        super.onPause();
         saveNote(false);
     }
 
