8dc77b91f11eb2dd10aa1f5c492bd08e733d0c31
Pavel Perestoronin <eigenein@gmail.com>
2015-11-04 00:13:53 +0100

    #101: upgrading database (partial work).

--- app/src/main/java/in/eigene/miary/persistence/DatabaseHelper.java
+++ app/src/main/java/in/eigene/miary/persistence/DatabaseHelper.java
@@ -3,11 +3,37 @@ package in.eigene.miary.persistence;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.util.SparseArray;
+
+import in.eigene.miary.helpers.lang.Consumer;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String LOG_TAG = DatabaseHelper.class.getSimpleName();
     private static final String NAME = "notes.db";
     private static final int VERSION = 1;
+
+    private static final SparseArray<Consumer<SQLiteDatabase>> UPGRADES = new SparseArray<>();
+
+    static {
+        UPGRADES.append(2, new Consumer<SQLiteDatabase>() {
+            /**
+             * Replaces legacy color codes with actual color values.
+             */
+            @Override
+            public void accept(final SQLiteDatabase db) {
+                db.execSQL(String.format("UPDATE %s SET color = %s WHERE color = %s;", Note.Contract.TABLE, 0xFFFFFF, 0)); // white
+                // TODO: LEGACY_COLORS.put(1, 0xFFEF5350); // red
+                // TODO: LEGACY_COLORS.put(2, 0xFFFFA726); // orange
+                // TODO: LEGACY_COLORS.put(3, 0xFFFFEB3B); // yellow
+                // TODO: LEGACY_COLORS.put(4, 0xFFF5F5F5); // gray
+                // TODO: LEGACY_COLORS.put(5, 0xFF8BC34A); // green
+                // TODO: LEGACY_COLORS.put(6, 0xFF90CAF9); // blue
+                // TODO: LEGACY_COLORS.put(7, 0xFFCE93D8); // purple
+            }
+        });
+    }
 
     public DatabaseHelper(final Context context) {
         super(context, NAME, null, VERSION);
@@ -38,6 +64,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(final SQLiteDatabase db, final int oldVersion, final int newVersion) {
-        // TODO.
+        Log.i(LOG_TAG, "Upgrading database from version " + oldVersion + " to version " + newVersion);
+        for (int version = oldVersion + 1; version <= newVersion; version += 1) {
+            Log.i(LOG_TAG, "Applying upgrade to version " + version);
+            UPGRADES.get(version).accept(db);
+        }
+        Log.i(LOG_TAG, "Upgrades finished.");
     }
 }
